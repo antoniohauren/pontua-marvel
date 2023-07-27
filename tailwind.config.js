1b61623ba284ee61c30369534742b97e5770245c
antoniohauren <antoniohauren@protonmail.com>
2023-07-27 10:30:33 -0300

    chore: vite with tailwind and storybook fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      divider: "#ebeff2",
+      "blue/800": "#00113d",
+      "blue/600": "#213770",
+      "blue/200": "#747d94",
+      "orange/500": "#f21a05",
+      "orange/400": "#f43724",
+      "gray/background": "#f5f6f8",
+      "gray/500": "#777777",
+      "gray/100": "#eaecf0",
+      "gray/400": "#b7b7b7",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
