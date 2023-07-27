09da5d0272620f5bc577c2f2a1a2481d5d85938e
antoniohauren <antoniohauren@protonmail.com>
2023-07-27 10:38:45 -0300

    chore: alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
@@ -10,4 +11,9 @@ export default defineConfig({
       exportAsDefault: true,
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
